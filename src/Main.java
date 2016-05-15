e725f6676d2f9ecae8e20bb3759b53d4488b32cc
StrizhakLena <strizhak_lena@mail.ru>
2016-05-15 16:00:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,41 @@
-/**
- * Created by User on 26.04.2016.
- */
 public class Main {
-}
     public static void main(String[] args) {
 
-        Land Land = new Land();
-        Land.setName("Kingdom1");
-        Land.setPosistiom("difmaid");
-       Land.setgiveLand(1);
+        betterKingdom betterKingdom = new betterKingdom();
+        betterKingdom.setroyalName("CNU");
+        betterKingdom.setroyalAddress("Shevchenko, 81");
+        betterKingdom.setgiveGoldenPicture(10);
 
-        Maid maid = new Maid ();
+       Kingdom kingdom = new Kingdom ();
 
-        maid.setSurname("Ivanova");
-        maid.setName("Lena");
-        maid.setPosistiom("gardener");
-                Land.addMaid(Maid);
+        kingdom.setnameOfKingdom("Magic");
+        kingdom.setnameOfKing("Rishard");
+        kingdom.setnameOfQueen("Mary");
+        kingdom.settheQuantityOfAdviser(5);
+        kingdom.settheQuantityOfMaid(40);
+        kingdom.setaddress("Island");
+        betterKingdom.addKingdom(kingdom);
 
-        maid = new Worker();
-        maid.setSurname("Sidorov");
-        maid.setName("Ivan");
-        maid.setPosistiom("shef");
-        Land.addMaid(Maid);
+        kingdom = new Kingdom();
+        kingdom.setnameOfKingdom("Shiny");
+        kingdom.setnameOfKing("David");
+        kingdom.setnameOfQueen("Dana");
+        kingdom.settheQuantityOfAdviser(2);
+        kingdom.settheQuantityOfMaid(30);
+        kingdom.setaddress("Norway");
+        betterKingdom.addKingdom(kingdom);
 
-        maid = new Worker();
-        maid.setSurname("Matveeva");
-        maid.setName("Ira");
-        maid.setPosistiom("cleaner");
-        Land.addMaid(Maid);
+        kingdom = new Kingdom();
+        kingdom.setnameOfKingdom("wonderful");
+        kingdom.setnameOfKing("Tom");
+        kingdom.setnameOfQueen("Vanessa");
+        kingdom.settheQuantityOfAdviser(1);
+        kingdom.settheQuantityOfMaid(35);
+        kingdom.setaddress("England");
+        betterKingdom.addKingdom(kingdom);
 
 
 
-
-        System.out.println(Land.toString());
+        System.out.println(betterKingdom.toString());
     }
 }
